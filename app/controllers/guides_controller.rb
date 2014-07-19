9feebb8e1eb473a43c510afd9f62550b8ce72ff9
moises <elnaufrago2009@hotmail.com>
2014-07-19 14:27:48 -0500

    base2

--- app/controllers/guides_controller.rb
+++ app/controllers/guides_controller.rb
@@ -1,4 +1,9 @@
 class GuidesController < InheritedResources::Base
+	def add_detail
+		@order = Guide.build	
+		@ci = Time.now.to_i
+		respond_with(@order, @ci)
+	end
 	def permitted_params
 		params.permit!
 	end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  resources :items
+
+  resources :guides do
+    collection { get :add_detail }
+  end  
 
   resources :guides
 
